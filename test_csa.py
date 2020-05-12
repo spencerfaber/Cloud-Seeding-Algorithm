3b2ce4756f69d7fa13db30cb723127ea71e22198
Spencer faber <sfaber34@gmail.com>
2020-05-12 10:04:28 -0600

    added comments to test_csa.py

--- test_csa.py
+++ test_csa.py
@@ -1,26 +1,22 @@
-import numpy as np
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
 from csa import csa
 
+# counter for main loop below
 count = 0
+
+# Read MIP, POPS, and CDP CSVs in test_data folder
 mip_data_df = pd.read_csv(r'test_data\mip_data.csv')
 pops_data_df = pd.read_csv(r'test_data\pops_data.csv')
 cdp_data_df = pd.read_csv(r'test_data\cdp_data.csv')
 
-foo = np.zeros((len(mip_data_df),9))
-
 while count < len(mip_data_df):
+    # Get one row of MIP, POPS, and CDP data. Convert row to python list (1D array)
     mip_data = mip_data_df.iloc[count, :].values.tolist()
     pops_data = pops_data_df.iloc[count, :].values.tolist()
     cdp_data = cdp_data_df.iloc[count, :].values.tolist()
 
-    bar = csa(mip_data, pops_data, cdp_data)
-    # foo[count,:] = bar
-    print(bar)
+    # Call the main cloud seeding algorithm function with MIP, POPS, and CDP data as inputs
+    csa_out = csa(mip_data, pops_data, cdp_data)
+    # Print what the cloud seeding algorithm returned
+    print(csa_out)
     count += 1
-
-plt.plot(foo[:,0],foo[:,6])
-plt.show()
-print('')
